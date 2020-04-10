98a53dc83b949eada83fdf3c6bac7b8933b056b8
Artyom Aleksyuk <artyom.h31@gmail.com>
2020-04-10 10:40:51 +0300

    Explicitly set port number, remove too verbose comments

--- tcp/server/main.c
+++ tcp/server/main.c
@@ -15,7 +15,11 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in serv_addr, cli_addr;
     ssize_t n;
 
-    /* First call to socket() function */
+    if (argc < 2) {
+        fprintf(stderr, "usage %s port\n", argv[0]);
+        exit(1);
+    }
+
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd < 0) {
@@ -23,28 +27,23 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    portno = (uint16_t) atoi(argv[1]);
+
     /* Initialize socket structure */
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    portno = 5001;
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    /* Now bind the host address using bind() call.*/
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
         exit(1);
     }
 
-    /* Now start listening for the clients, here process will
-       * go in sleep mode and will wait for the incoming connection
-    */
-
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
-    /* Accept actual connection from the client */
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
     if (newsockfd < 0) {
@@ -52,7 +51,6 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    /* If connection is established then start communicating */
     bzero(buffer, 256);
     n = read(newsockfd, buffer, 255); // recv on Windows
 
@@ -63,7 +61,6 @@ int main(int argc, char *argv[]) {
 
     printf("Here is the message: %s\n", buffer);
 
-    /* Write a response to the client */
     n = write(newsockfd, "I got your message", 18); // send on Windows
 
     if (n < 0) {
